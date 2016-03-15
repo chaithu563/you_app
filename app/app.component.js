885dbe09f30878b0a9c508bbff2d26d9e0b5c7d5
chaitanya edara <chaitanya.eedara@gmail.com>
2016-03-15 08:00:45 +0530

    http working error

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './content/header/header.component', './content/body/body.component', './httpclient'], function(exports_1) {
+System.register(['angular2/core', './content/header/header.component', './content/body/body.component', 'angular2/http'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,7 +8,7 @@ System.register(['angular2/core', './content/header/header.component', './conten
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, header_component_1, body_component_1, httpclient_1;
+    var core_1, header_component_1, body_component_1, http_1;
     var AppComponent;
     return {
         setters:[
@@ -21,23 +21,29 @@ System.register(['angular2/core', './content/header/header.component', './conten
             function (body_component_1_1) {
                 body_component_1 = body_component_1_1;
             },
-            function (httpclient_1_1) {
-                httpclient_1 = httpclient_1_1;
+            function (http_1_1) {
+                http_1 = http_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
-                function AppComponent(httpClient) {
-                    httpClient.get('https://www.googleapis.com/youtube/v3/playlists')
+                //constructor(httpClient: HttpClient) {
+                //    httpClient.get('https://www.googleapis.com/youtube/v3/playlists')
+                //        .map(res => res.json())
+                //        .subscribe(res => console.log(res));
+                //}
+                function AppComponent(http) {
+                    http.get('https://www.googleapis.com/youtube/v3/playlists')
                         .map(function (res) { return res.json(); })
-                        .subscribe(function (res) { return console.log(res); });
+                        .subscribe(function (people) { return console.log(people); });
                 }
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'hp-app',
+                        viewProviders: [http_1.HTTP_PROVIDERS],
                         template: '<hp-header></hp-header><hp-body></hp-body>',
                         directives: [header_component_1.HeaderComponent, body_component_1.BodyComponent]
                     }), 
-                    __metadata('design:paramtypes', [httpclient_1.HttpClient])
+                    __metadata('design:paramtypes', [http_1.Http])
                 ], AppComponent);
                 return AppComponent;
             })();
--- app/app.component.ts
+++ app/app.component.ts
@@ -6,17 +6,27 @@ import {HttpClient} from './httpclient';
 
 @Component({
     selector: 'hp-app',
+    viewProviders: [HTTP_PROVIDERS],
     template: '<hp-header></hp-header><hp-body></hp-body>',
     directives: [HeaderComponent, BodyComponent]
 })
 export class AppComponent {
     
 
-    constructor(httpClient: HttpClient) {
-        httpClient.get('https://www.googleapis.com/youtube/v3/playlists')
+    //constructor(httpClient: HttpClient) {
+    //    httpClient.get('https://www.googleapis.com/youtube/v3/playlists')
            
-            .map(res => res.json())
+    //        .map(res => res.json())
             
-            .subscribe(res => console.log(res));
+    //        .subscribe(res => console.log(res));
+    //}
+
+    constructor(http: Http) {
+        http.get('https://www.googleapis.com/youtube/v3/playlists')
+            // Call map on the response observable to get the parsed people object
+            .map(res => res.json())
+            // Subscribe to the observable to get the parsed people object and attach it to the
+            // component
+            .subscribe(people => console.log(people));
     }
 }
