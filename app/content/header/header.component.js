bfbd51da810243e72d5f7af5825c7c0801699475
chaitanya edara <chaitanya.edara@aveva.com>
2016-03-21 16:39:40 +0530

    header http://valor-software.com/ng2-bootstrap/  carousel

--- app/content/header/header.component.js
+++ app/content/header/header.component.js
@@ -24,6 +24,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 HeaderComponent = __decorate([
                     core_1.Component({
                         selector: 'hp-header',
+                        //	moduleId: $__moduleContext.id,
                         // template: '<h1>My Header1</h1> <input type="text" />'
                         templateUrl: 'app/content/header/header.component.html',
                         styleUrls: ['app/content/header/header.component.css']
--- app/content/header/header.component.ts
+++ app/content/header/header.component.ts
@@ -1,9 +1,15 @@
 ﻿import { Component} from 'angular2/core';
 
 @Component({
-    selector: 'hp-header',
+	selector: 'hp-header',
+//	moduleId: $__moduleContext.id,
    // template: '<h1>My Header1</h1> <input type="text" />'
     templateUrl: 'app/content/header/header.component.html',
     styleUrls: ['app/content/header/header.component.css']
+
+	//templateUrl: 'header.component.html',
+    //styleUrls: ['header.component.css']
 })
+
+	//http://valor-software.com/ng2-bootstrap/
 export class HeaderComponent { }
